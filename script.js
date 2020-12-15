6949a88021d96ee8c83ffa8bc425e0775589dc3c
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-14 17:10:06 -0700

    stopping the random cycle

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ textarea.focus()
 
 textarea.addEventListener('keyup', (e) => {
   createTags(e.target.value)
+  if (e.key === 'Enter') {
+    setTimeout(() => {
+      e.target.value = ''
+    }, 10)
+    randomSelect()
+  }
 })
 
 function createTags(input) {
@@ -17,4 +23,40 @@ function createTags(input) {
     tagElement.innerText = tag
     tagsElement.appendChild(tagElement)
   })
+}
+
+function randomSelect() {
+  const times = 30
+
+  const interval = setInterval(() => {
+    const randomTag = pickRandomTag()
+    hightlightTag(randomTag)
+    setTimeout(() => {
+      unHighlightTag(randomTag)
+    }, 100)
+  }, 100);
+
+  setTimeout(() => {
+    clearInterval(interval)
+
+    setTimeout(() => {
+      const randomTag = pickRandomTag()
+      hightlightTag(randomTag)
+    }, 100)
+
+  }, times * 100)
+
+}
+
+function pickRandomTag() {
+  const tags = document.querySelectorAll('.tag')
+  return tags[Math.floor(Math.random() * tags.length)]
+}
+
+function hightlightTag(tag) {
+  tag.classList.add('highlight')
+}
+
+function unHighlightTag(tag) {
+  tag.classList.remove('highlight')
 }
