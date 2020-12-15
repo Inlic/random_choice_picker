6c84cbdb82aef452ec96a5e16782a117935d4469
Ben Wiedeback <66647604+Inlic@users.noreply.github.com>
2020-12-14 17:01:12 -0700

    script to add elements as we type

---
+++ script.js
@@ -0,0 +1,20 @@
+const tagsElement = document.getElementById('tags')
+const textArea = document.getElementById('textarea')
+
+textarea.focus()
+
+textarea.addEventListener('keyup', (e) => {
+  createTags(e.target.value)
+})
+
+function createTags(input) {
+  const tags = input.split(',').filter(tag => tag.trim() !== '').map(tag => tag.trim())
+  tagsElement.innerHTML = ''
+
+  tags.forEach(tag => {
+    const tagElement = document.createElement('span')
+    tagElement.classList.add('tag')
+    tagElement.innerText = tag
+    tagsElement.appendChild(tagElement)
+  })
+}
